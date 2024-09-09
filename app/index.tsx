6f0e7dccccbdbd647b19483a757c9e93484af1de
Ulaş Demir <92229764+ulasdemir472@users.noreply.github.com>
2024-09-09 18:32:05 +0300

    initial setup completed

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const App = () => {
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
